49d070fd5d865d3909294b9c0fde7e3b14b23e7b
sejoonable <89341263+sejoonable@users.noreply.github.com>
2023-01-25 18:29:48 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,8 @@
+function solution(my_string, num1, num2) {
+    let result = [...my_string];
+    result.splice(num1, 1, my_string[num2])
+    result.splice(num2, 1, my_string[num1])
+    return result.join("");
+}
+
+
