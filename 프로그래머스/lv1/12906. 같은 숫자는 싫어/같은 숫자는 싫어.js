b89d8d90cba8cd584fecfae9841a125bdd45cb2b
sejoonable <89341263+sejoonable@users.noreply.github.com>
2023-01-27 14:24:06 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 37.64 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    let a = [];
+
+    for (let i = 0; i < arr.length; i++) {
+       if ((arr[i] !== arr[i+1])) {
+           a.push(arr[i])
+       }
+    } return a
+}
+
+    
+
