9e9a633b0b12787bfde27110c75b0def0168713a
sejoonable <89341263+sejoonable@users.noreply.github.com>
2023-01-28 10:31:34 +0900

    [level 1] Title: 소수 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,18 @@
+function solution(nums) {
+  let sosoo = [];
+for(let i = 0; i<nums.length; i++) {
+    for(let j = i + 1; j < nums.length; j++) {
+        for(let k = j + 1; k < nums.length; k++) {
+            if (isSosoo(nums[i], nums[j], nums[k])) sosoo.push('a')
+            }
+            
+        }
+    } 
+    return sosoo.length;
+}
+
+function isSosoo(x,y,z) {
+    for (let l = 2; l < (x + y + z); l++) {
+               if ((x + y + z) % l == 0) return false;
+} return true;
+}
