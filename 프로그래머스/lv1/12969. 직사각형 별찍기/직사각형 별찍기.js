85d9416ee4038003d6198ead6651e1ad6a6c2e1d
sejoonable <89341263+sejoonable@users.noreply.github.com>
2023-01-30 15:56:17 +0900

    [level 1] Title: 직사각형 별찍기, Time: 72.33 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -2,12 +2,6 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', data => {
     const n = data.split(" ");
     const a = Number(n[0]), b = Number(n[1]);
-
-let garo = '*'.repeat(a)
-
-for (let i = 1; i <= b; i++) {
-    console.log(garo)
-}
-
-    
+    console.log(a);
+    console.log(b);
 });
