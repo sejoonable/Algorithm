8d5e8bbf827e4346eaf3482b8b9af6016434740d
sejoonable <89341263+sejoonable@users.noreply.github.com>
2023-02-08 20:01:47 +0900

    [level 0] Title: 평행, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.js
+++ 프로그래머스/lv0/120875. 평행/평행.js
@@ -1,17 +1,15 @@
 function solution(dots) {
-    let a = []
-    
-    for (let i = 0; i < dots.length-1; i++) {
-        for (let j = i+1; j < dots.length; j++) {
-            let A = (dots[i][1] - dots[j][1]) / (dots[i][0] - dots[j][0])
-            
-            if(a.includes(A)) return 1
-            a.push(A)
-            console.log(a)
+    let a = [];
+    let giulgi;
+    for(let i = 0; i < dots.length-1; i++) {
+        for(let j = i+1; j < dots.length; j++) {
+        giulgi = (dots[i][1] - dots[j][1]) / (dots[i][0] - dots[j][0])
+        
+        if(a.includes(giulgi)) return 1
+        a.push(giulgi)
         }
     } return 0
 }
-
 
 
 // 다돌려서 기울기만 같으면 된다.
