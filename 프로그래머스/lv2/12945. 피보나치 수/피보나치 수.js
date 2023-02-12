15a4a2e6589bd505d336d3d53333186b67901741
sejoonable <89341263+sejoonable@users.noreply.github.com>
2023-02-12 10:51:04 +0900

    [level 2] Title: 피보나치 수, Time: 4.40 ms, Memory: 39.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let F = [0, 1];
+    
+    for (let i = 2; i <= n; i++) {
+        F.push((F[i-2] + F[i-1]) % 1234567)        
+    } 
+    let A = F[n] % 1234567
+    return A
+}
